e49bf55c28291ba7f0523f612fbdcc5317b59b50
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-10-04 09:05:36 +0530

    add spring security configuration and JwtTokenValidator logic

---
+++ backend/src/main/java/com/rahul/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.rahul.config;
+
+public class JwtConstant {
+    public static final String SECRETE_KEY = "skjgsjhs sjkghfj d fjdjhkfh ";
+    public static final String JWT_HEADER = "Authorization";
+}
