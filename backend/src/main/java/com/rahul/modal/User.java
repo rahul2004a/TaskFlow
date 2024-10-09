721eebe21c065775bfaba651559ba4cec09523de
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-10-09 19:32:50 +0530

    add User modal and UserRepository

---
+++ backend/src/main/java/com/rahul/modal/User.java
@@ -0,0 +1,22 @@
+package com.rahul.modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "assignee",cascade = CascadeType.ALL)
+    private List<Issue> assignedIssues = new ArrayList<>();
+    private int projectSize;
+}
